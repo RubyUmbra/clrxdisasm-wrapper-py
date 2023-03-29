79667604eaf42150066263ffdd296a09e545c8e1
RubyUmbra <rubyumbra@gmail.com>
2023-03-30 01:30:37 +0300

    Upgrade compute_pgm_rsrc[1-3] unpacking

--- clrxdisasm_wrapper/kernel_descriptor.py
+++ clrxdisasm_wrapper/kernel_descriptor.py
@@ -1,121 +1,73 @@
 from struct import unpack
-from utils import Bits
+from construct import Construct, BitStruct, ByteSwapped, BitsInteger, Padding
 
 
-class ComputePgmRsrc1:
-    def __init__(self, compute_pgm_rsrc1: bytes):
-        bits: Bits = Bits(unpack("<I", compute_pgm_rsrc1)[0], 32)
-        self.FWD_PROGRESS: int = bits.pop_end(1)
-        self.MEM_ORDERED: int = bits.pop_end(1)
-        self.WGP_MODE: int = bits.pop_end(1)
-        _: int = bits.pop_end(2)
-        self.FP16_OVFL: int = bits.pop_end(1)
-        self.CDBG_USER: int = bits.pop_end(1)
-        self.BULKY: int = bits.pop_end(1)
-        self.ENABLE_IEEE_MODE: int = bits.pop_end(1)
-        self.DEBUG_MODE: int = bits.pop_end(1)
-        self.ENABLE_DX10_CLAMP: int = bits.pop_end(1)
-        self.PRIV: int = bits.pop_end(1)
-        self.FLOAT_DENORM_MODE_16_64: int = bits.pop_end(2)
-        self.FLOAT_DENORM_MODE_32: int = bits.pop_end(2)
-        self.FLOAT_ROUND_MODE_16_64: int = bits.pop_end(2)
-        self.FLOAT_ROUND_MODE_32: int = bits.pop_end(2)
-        self.PRIORITY: int = bits.pop_end(2)
-        self.GRANULATED_WAVEFRONT_SGPR_COUNT: int = bits.pop_end(4)
-        self.GRANULATED_WORKITEM_VGPR_COUNT: int = bits.pop_end(6)
-        assert bits.size == 0
+class ComputePgmRsrc:
+    FORMAT: Construct
+
+    def __init__(self, data: bytes):
+        self.__struct = self.FORMAT.parse(data)
+
+    def __getitem__(self, item):
+        return self.__struct.__getitem__(item)
 
     def __str__(self):
-        bits: Bits = Bits()
-        bits.push_end(self.GRANULATED_WORKITEM_VGPR_COUNT, 6)
-        bits.push_end(self.GRANULATED_WAVEFRONT_SGPR_COUNT, 4)
-        bits.push_end(self.PRIORITY, 2)
-        bits.push_end(self.FLOAT_ROUND_MODE_32, 2)
-        bits.push_end(self.FLOAT_ROUND_MODE_16_64, 2)
-        bits.push_end(self.FLOAT_DENORM_MODE_32, 2)
-        bits.push_end(self.FLOAT_DENORM_MODE_16_64, 2)
-        bits.push_end(self.PRIV, 1)
-        bits.push_end(self.ENABLE_DX10_CLAMP, 1)
-        bits.push_end(self.DEBUG_MODE, 1)
-        bits.push_end(self.ENABLE_IEEE_MODE, 1)
-        bits.push_end(self.BULKY, 1)
-        bits.push_end(self.CDBG_USER, 1)
-        bits.push_end(self.FP16_OVFL, 1)
-        bits.push_end(0, 2)
-        bits.push_end(self.WGP_MODE, 1)
-        bits.push_end(self.MEM_ORDERED, 1)
-        bits.push_end(self.FWD_PROGRESS, 1)
-        assert bits.size == 32
-        return f"0x{bits.num:0>8x}"
+        return f'0x{unpack("<I", self.FORMAT.build(self.__struct))[0]:0>8x}'
 
 
-class ComputePgmRsrc2:
-    def __init__(self, compute_pgm_rsrc2: bytes):
-        bits: Bits = Bits(unpack("<I", compute_pgm_rsrc2)[0], 32)
-        _: int = bits.pop_end(1)
-        self.ENABLE_EXCEPTION_INT_DIVIDE_BY_ZERO: int = bits.pop_end(1)
-        self.ENABLE_EXCEPTION_IEEE_754_FP_INEXACT: int = bits.pop_end(1)
-        self.ENABLE_EXCEPTION_IEEE_754_FP_UNDERFLOW: int = bits.pop_end(1)
-        self.ENABLE_EXCEPTION_IEEE_754_FP_OVERFLOW: int = bits.pop_end(1)
-        self.ENABLE_EXCEPTION_IEEE_754_FP_DIVISION_BY_ZERO: int = bits.pop_end(1)
-        self.ENABLE_EXCEPTION_FP_DENORMAL_SOURCE: int = bits.pop_end(1)
-        self.ENABLE_EXCEPTION_IEEE_754_FP_INVALID_OPERATION: int = bits.pop_end(1)
-        self.GRANULATED_LDS_SIZE: int = bits.pop_end(9)
-        self.ENABLE_EXCEPTION_MEMORY: int = bits.pop_end(1)
-        self.ENABLE_EXCEPTION_ADDRESS_WATCH: int = bits.pop_end(1)
-        self.ENABLE_VGPR_WORKITEM_ID: int = bits.pop_end(2)
-        self.ENABLE_SGPR_WORKGROUP_INFO: int = bits.pop_end(1)
-        self.ENABLE_SGPR_WORKGROUP_ID_Z: int = bits.pop_end(1)
-        self.ENABLE_SGPR_WORKGROUP_ID_Y: int = bits.pop_end(1)
-        self.ENABLE_SGPR_WORKGROUP_ID_X: int = bits.pop_end(1)
-        self.ENABLE_TRAP_HANDLER: int = bits.pop_end(1)
-        self.USER_SGPR_COUNT: int = bits.pop_end(5)
-        self.ENABLE_PRIVATE_SEGMENT: int = bits.pop_end(1)
-        assert bits.size == 0
-
-    def __str__(self):
-        bits: Bits = Bits()
-        bits.push_end(self.ENABLE_PRIVATE_SEGMENT, 1)
-        bits.push_end(self.USER_SGPR_COUNT, 5)
-        bits.push_end(self.ENABLE_TRAP_HANDLER, 1)
-        bits.push_end(self.ENABLE_SGPR_WORKGROUP_ID_X, 1)
-        bits.push_end(self.ENABLE_SGPR_WORKGROUP_ID_Y, 1)
-        bits.push_end(self.ENABLE_SGPR_WORKGROUP_ID_Z, 1)
-        bits.push_end(self.ENABLE_SGPR_WORKGROUP_INFO, 1)
-        bits.push_end(self.ENABLE_VGPR_WORKITEM_ID, 2)
-        bits.push_end(self.ENABLE_EXCEPTION_ADDRESS_WATCH, 1)
-        bits.push_end(self.ENABLE_EXCEPTION_MEMORY, 1)
-        bits.push_end(self.GRANULATED_LDS_SIZE, 9)
-        bits.push_end(self.ENABLE_EXCEPTION_IEEE_754_FP_INVALID_OPERATION, 1)
-        bits.push_end(self.ENABLE_EXCEPTION_FP_DENORMAL_SOURCE, 1)
-        bits.push_end(self.ENABLE_EXCEPTION_IEEE_754_FP_DIVISION_BY_ZERO, 1)
-        bits.push_end(self.ENABLE_EXCEPTION_IEEE_754_FP_OVERFLOW, 1)
-        bits.push_end(self.ENABLE_EXCEPTION_IEEE_754_FP_UNDERFLOW, 1)
-        bits.push_end(self.ENABLE_EXCEPTION_IEEE_754_FP_INEXACT, 1)
-        bits.push_end(self.ENABLE_EXCEPTION_INT_DIVIDE_BY_ZERO, 1)
-        bits.push_end(0, 1)
-        assert bits.size == 32
-        return f"0x{bits.num:0>8x}"
+class ComputePgmRsrc1(ComputePgmRsrc):
+    FORMAT = ByteSwapped(BitStruct(
+        "GRANULATED_WORKITEM_VGPR_COUNT" / BitsInteger(6),
+        "GRANULATED_WAVEFRONT_SGPR_COUNT" / BitsInteger(4),
+        "PRIORITY" / BitsInteger(2),
+        "FLOAT_ROUND_MODE_32" / BitsInteger(2),
+        "FLOAT_ROUND_MODE_16_64" / BitsInteger(2),
+        "FLOAT_DENORM_MODE_32" / BitsInteger(2),
+        "FLOAT_DENORM_MODE_16_64" / BitsInteger(2),
+        "PRIV" / BitsInteger(1),
+        "ENABLE_DX10_CLAMP" / BitsInteger(1),
+        "DEBUG_MODE" / BitsInteger(1),
+        "ENABLE_IEEE_MODE" / BitsInteger(1),
+        "BULKY" / BitsInteger(1),
+        "CDBG_USER" / BitsInteger(1),
+        "FP16_OVFL" / BitsInteger(1),
+        Padding(2),
+        "WGP_MODE" / BitsInteger(1),
+        "MEM_ORDERED" / BitsInteger(1),
+        "FWD_PROGRESS" / BitsInteger(1),
+    ))
 
 
-class ComputePgmRsrc3:
-    def __init__(self, compute_pgm_rsrc3: bytes):
-        bits: Bits = Bits(unpack("<I", compute_pgm_rsrc3)[0], 32)
-        self.IMAGE_OP: int = bits.pop_end(1)
-        _: int = bits.pop_end(19)
-        self.TRAP_ON_END: int = bits.pop_end(1)
-        self.TRAP_ON_START: int = bits.pop_end(1)
-        self.INST_PREF_SIZE: int = bits.pop_end(6)
-        self.SHARED_VGPR_COUNT: int = bits.pop_end(4)
-        assert bits.size == 0
+class ComputePgmRsrc2(ComputePgmRsrc):
+    FORMAT = ByteSwapped(BitStruct(
+        "ENABLE_PRIVATE_SEGMENT" / BitsInteger(1),
+        "USER_SGPR_COUNT" / BitsInteger(5),
+        "ENABLE_TRAP_HANDLER" / BitsInteger(1),
+        "ENABLE_SGPR_WORKGROUP_ID_X" / BitsInteger(1),
+        "ENABLE_SGPR_WORKGROUP_ID_Y" / BitsInteger(1),
+        "ENABLE_SGPR_WORKGROUP_ID_Z" / BitsInteger(1),
+        "ENABLE_SGPR_WORKGROUP_INFO" / BitsInteger(1),
+        "ENABLE_VGPR_WORKITEM_ID" / BitsInteger(2),
+        "ENABLE_EXCEPTION_ADDRESS_WATCH" / BitsInteger(1),
+        "ENABLE_EXCEPTION_MEMORY" / BitsInteger(1),
+        "GRANULATED_LDS_SIZE" / BitsInteger(9),
+        "ENABLE_EXCEPTION_IEEE_754_FP_INVALID_OPERATION" / BitsInteger(1),
+        "ENABLE_EXCEPTION_FP_DENORMAL_SOURCE" / BitsInteger(1),
+        "ENABLE_EXCEPTION_IEEE_754_FP_DIVISION_BY_ZERO" / BitsInteger(1),
+        "ENABLE_EXCEPTION_IEEE_754_FP_OVERFLOW" / BitsInteger(1),
+        "ENABLE_EXCEPTION_IEEE_754_FP_UNDERFLOW" / BitsInteger(1),
+        "ENABLE_EXCEPTION_IEEE_754_FP_INEXACT" / BitsInteger(1),
+        "ENABLE_EXCEPTION_INT_DIVIDE_BY_ZERO" / BitsInteger(1),
+        Padding(1),
+    ))
 
-    def __str__(self):
-        bits: Bits = Bits()
-        bits.push_end(self.SHARED_VGPR_COUNT, 4)
-        bits.push_end(self.INST_PREF_SIZE, 6)
-        bits.push_end(self.TRAP_ON_START, 1)
-        bits.push_end(self.TRAP_ON_END, 1)
-        bits.push_end(0, 19)
-        bits.push_end(self.IMAGE_OP, 1)
-        assert bits.size == 32
-        return f"0x{bits.num:0>8x}"
+
+class ComputePgmRsrc3(ComputePgmRsrc):
+    FORMAT = ByteSwapped(BitStruct(
+        "SHARED_VGPR_COUNT" / BitsInteger(4),
+        "INST_PREF_SIZE" / BitsInteger(6),
+        "TRAP_ON_START" / BitsInteger(1),
+        "TRAP_ON_END" / BitsInteger(1),
+        Padding(19),
+        "IMAGE_OP" / BitsInteger(1),
+    ))
--- clrxdisasm_wrapper/utils.py
+++ clrxdisasm_wrapper/utils.py
@@ -2,26 +2,6 @@ import os
 from contextlib import contextmanager
 
 
-class Bits:
-    def __init__(self, num: int = 0, size: int = 0):
-        self.num: int = num
-        self.size: int = size
-
-    def push_end(self, value: int, size: int):
-        self.num <<= size
-        self.num |= value & ((1 << size) - 1)
-        self.size += size
-
-    def pop_end(self, size: int) -> int:
-        value = self.num & ((1 << size) - 1)
-        self.num >>= size
-        self.size -= size
-        return value
-
-    def __str__(self):
-        return f"0b{self.num:0{self.size}b}"
-
-
 @contextmanager
 def temp_file(filename: str):
     try:
